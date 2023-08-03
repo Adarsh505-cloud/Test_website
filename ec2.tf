b5ffb71950af75674c8e26922913c8272d8e6aa8
root <root@DESKTOP-7HLL80K.localdomain>
2023-08-03 19:21:06 +0530

    ec22

--- ec2.tf
+++ ec2.tf
@@ -2,29 +2,12 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_security_group" "default" {
-  name = "my-security-group"
-  description = "A security group for my EC2 instance"
-  vpc_id = "vpc-0434f51d8a18191d5"
-
-  ingress {
-    protocol = "tcp"
-    port = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    protocol = "tcp"
-    port = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 resource "aws_instance" "default" {
   ami = "ami-089ffe32605122764"
   instance_type = "t2.micro"
   key_name = "af-adarsh"
-  security_groups = [aws_security_group.default.id]
+  security_groups = ["sg-0c6c3f7c75ba8425b"]
   subnet_id = "subnet-06b35b3d07ceec3e2"
 }
 
